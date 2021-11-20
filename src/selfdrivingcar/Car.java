b28adb7079e9687aa11bbe58f9205def3804eb8e
sandy <sandy@192.168.0.34>
2021-11-19 22:46:13 -0500

    updates

--- src/selfdrivingcar/Car.java
+++ src/selfdrivingcar/Car.java
@@ -1,17 +1,44 @@
 package selfdrivingcar;
 
+import edu.fiu.sysdesign.SelfCheckUtils;
+
 public class Car {
 	private String brand;
 	private String color;
 	private String lights;
-
+	private String powerNoise;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	car = new Car ();
 	}
 	private void lightsOn() {}
 	private void lightsOff() {}
 	private void carOn() {}
 	private void carOff() {} 
+	private void powerNoise() {}
+}
+
+
+	public Car () {}
+	
+	
+
+
+@Override
+public boolean selfCheck() {
+	// TODO Auto-generated method stub
+	return SelfCheckUtils.randomCheck(0.1);
+}
+
+@Override
+public String getComponentName() {
+	// TODO Auto-generated method stub
+	return name;
+}
+
+@Override
+public boolean runSelfCheck() {
+	// TODO Auto-generated method stub
+	return SelfCheckUtils.basicSelfCheckRunner(this);
 }
 
